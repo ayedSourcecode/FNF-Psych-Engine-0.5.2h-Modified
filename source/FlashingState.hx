ad203feec9d528cfe9c3b83aa849a5409c01720e
Tie-Guo <117576846+Tie-Guo@users.noreply.github.com>
2022-12-27 12:19:26 +0800

    Add files via upload

--- source/FlashingState.hx
+++ source/FlashingState.hx
@@ -24,18 +24,18 @@ class FlashingState extends MusicBeatState
 		add(bg);
 
 		warnText = new FlxText(0, 0, FlxG.width,
-			"Hey, watch out!\n
-			This Mod contains some flashing lights!\n
-			Press ENTER to disable them now or go to Options Menu.\n
-			Press ESCAPE to ignore this message.\n
-			You've been warned!",
+			"Hey, Bro!\n
+			This Engine made by TieGuo.\n
+			And it's modded by Psych Engine.\n
+			It's. not a true Engine!\n
+			Pressed A to continue",
 			32);
 		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
 		warnText.screenCenter(Y);
 		add(warnText);
 
                 #if android
-	        addVirtualPad(NONE, A_B);
+	        addVirtualPad(NONE, A);
                 #end
 	}
 
